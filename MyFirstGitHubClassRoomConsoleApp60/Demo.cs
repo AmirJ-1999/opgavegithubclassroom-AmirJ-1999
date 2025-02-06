d3fd8158d67355e1e8f6a72de0d7d91ca4253cf0
Amir Jawad <amirjawad79@gmail.com>
2025-02-06 13:09:45 +0100

    Added properties to demoFork class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class demoFork
+{
+    public string age { get; set; }
+    public string description { get; set; }
+
+    public string fork { get; set; }
+
+}
